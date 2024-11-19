379559a67527442af2ed4e3937bb3c87552b6e74
Robin Bergewski <robinbergewski@gmail.com>
2024-11-19 17:17:17 +0100

    feat: improve error handling
    - better handle initial gitlab connection errors
    - better handle invalid config files

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 #![feature(more_qualified_paths)]
 use std::env;
 use std::fs;
+use std::process;
 
 use clap::{Parser, Subcommand};
 
@@ -30,15 +31,37 @@ enum Command {
 async fn main() {
     let cli = Cli::parse();
 
-    let config_file = env::var("HOME").expect("$HOME not set") + "/.config/gmon/config";
-    let config_str = fs::read_to_string(config_file).expect("no config file");
-    let config: Config = toml::from_str(&config_str).expect("invalid config file");
+    let config_file = env::var("HOME").expect("$HOME not set") + "/.config/gmon/config.toml";
+    let config_str = match fs::read_to_string(&config_file) {
+        Ok(s) => s,
+        Err(s) => {
+            eprintln!("failed reading config {} due to: {}", &config_file, &s);
+            process::exit(2);
+        },
+    };
+    let config: Config = match toml::from_str(&config_str) {
+        Ok(c) => c,
+        Err(e) => {
+            eprintln!("failed loading config {} due to: {}", &config_file, e.message());
+            process::exit(2);
+        },
+    };
 
-    let gapi = gitlab::GitlabBuilder::new(config.host, config.token)
+    let gapi = match gitlab::GitlabBuilder::new(config.host, config.token)
         .cert_insecure()
         .build_async()
-        .await
-        .expect("gitlab err");
+        .await {
+
+            Ok(api) => api,
+            Err(err) => {
+                eprintln!("connection to gitlab failed due to: {}", err);
+                process::exit(1);
+            },
+    };
+
+    std::panic::set_hook(Box::new(move |_| {
+        ratatui::restore();
+    }));
 
     match &cli.command {
         Command::Pipelines(args) => {
@@ -48,8 +71,5 @@ async fn main() {
             cmds::pipeline::run(gapi, args).await;
         }
     }
-    std::panic::set_hook(Box::new(move |_| {
-        ratatui::restore();
-    }));
     ratatui::restore();
 }
